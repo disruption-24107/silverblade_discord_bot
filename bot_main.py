0949b1834e99b3af4f51b7e58f03c4070ba7e5a9
Christopher Webb <31657038+cwebbtw@users.noreply.github.com>
2019-12-04 17:21:41 +0000

    Update bot_main.py

--- bot_main.py
+++ bot_main.py
@@ -1,8 +1,33 @@
 import discord
 import sys
 from discord.ext import commands
+from datetime import datetime
 
 bot = commands.Bot(command_prefix='!')
+
+applicants = {}
+
+
+@bot.event
+async def on_member_join(member):
+    await member.send("""
+Silverblade is a World of Warcraft mythic raiding guild, formed in 2016 after a number of casual but dedicated players wanted to spend a few nights per week raiding together. We've been steadily growing our list of cutting edge achievements from Xavius in Emerald Nightmare, to Argus in Antorus, G'huun in Uldir and Jaina Proudmore in Battle of Dazar'alor.
+
+We're currently progressing Azshara's Eternal Palace and have killed 7/8 bosses on mythic difficulty.
+
+We raid every Sunday and Thursday from 20:30 game time and about 3-3½ hours. There are regular mythic+ runs, rated battleground events and casual heroic raids after the initial progression run.
+
+If raiding with us is something you're interested in, hit the apply button and tell us why! We're always happy to read through applications! You can also contact any one of us in-game, and we'll make sure your request is directed to the right people so we can meet!
+
+Please take a look at the !rules before applying""")
+
+    await member.send("""
+** **
+If you are looking to make an application, please type `!application` and our friendly bot will send you everything you need to know.
+
+If you are a friend of the guild, just ask your friend to type `!friend @you` in one of the channels they have access to.
+""")
+
 
 @bot.command()
 @commands.cooldown(1, 180, commands.BucketType.user)
@@ -13,12 +38,12 @@ async def application(ctx):
     await ctx.author.send("""\
 *Thanks for your interest in applying!*
 
-We're going to need some information from you, but make sure you've read the **!rules** first. 
+We're going to need some information from you, but make sure you've read the **!rules** first.
 
 Once you've read the rules, please provide us with the following:
 ```
 !armory      The link to your armory profile (repeatable for each alt if you have alts)
-!raider.io   The link to your raider.io profile
+!raiderio   The link to your raider.io profile
 !logs        A link to your best logs
 ```
 It would be great if you could also tell us:
@@ -30,42 +55,85 @@ It would be great if you could also tell us:
 Once you've provided all of this tell me you're `!done`, I'll have someone from our recruitment team get back to you really soon! :-)
 """)
 
-applicants = []
+def applicant(applicant_map, member):
+    if member not in applicant_map:
+        applicant_map[member] = {}
+
+    return applicant_map[member]
+
 
 @bot.command()
+@commands.cooldown(1, 1, commands.BucketType.user)
+async def armory(ctx):
+    if isinstance(ctx.channel, discord.DMChannel):
+        applicant(applicants, ctx.author)["armory"] = ctx.message.content.split(None, 1)[1]
+        await ctx.author.send("""Thanks!""")
+
+@bot.command()
+@commands.cooldown(1, 1, commands.BucketType.user)
+async def raiderio(ctx):
+    if isinstance(ctx.channel, discord.DMChannel):
+        applicant(applicants, ctx.author)["raiderio"] = ctx.message.content.split(None, 1)[1]
+        await ctx.author.send("""Thanks!""")
+
+@bot.command()
+@commands.cooldown(1, 1, commands.BucketType.user)
+async def logs(ctx):
+    if isinstance(ctx.channel, discord.DMChannel):
+        applicant(applicants, ctx.author)["logs"] = ctx.message.content.split(None, 1)[1]
+        await ctx.author.send("""Thanks!""")
+
+@bot.command()
+@commands.cooldown(1, 1, commands.BucketType.user)
+async def why(ctx):
+    if isinstance(ctx.channel, discord.DMChannel):
+        applicant(applicants, ctx.author)["why"] = ctx.message.content.split(None, 1)[1]
+        await ctx.author.send("""Thanks!""")
+
+@bot.command()
+@commands.cooldown(1, 1, commands.BucketType.user)
+async def xp(ctx):
+    if isinstance(ctx.channel, discord.DMChannel):
+        applicant(applicants, ctx.author)["xp"] = ctx.message.content.split(None, 1)[1]
+        await ctx.author.send("""Thanks!""")
+
+@bot.command()
+@commands.cooldown(1, 180, commands.BucketType.user)
 async def done(ctx):
     if isinstance(ctx.channel, discord.DMChannel):
-        if ctx.author in applicants:
+        if "done" in applicant(applicants, ctx.author):
             await ctx.author.send("""You've already applied, check #applications to see your full application.""")
             return
 
-        applicants.append(ctx.author)
+        role = discord.utils.get(bot.get_guild(238705194244898817).roles, name='Applicant')
+        ctx.author.add_roles(role)
+
+        applicant(applicants, ctx.author)["done"] = datetime.now().strftime("%D")
 
         await ctx.author.send("""Great! I'll post your details in #applications, head over there to check the status""")
         channel = bot.get_channel(651182846207197185)
-        await channel.send("""**Application**
+        await channel.send("""**Application** ({0})
     
-*Name*: {0}
-*Armory*: http://not-implemented/
-*RaiderIO*: http://not-implemented/
-*Logs*: http://not-implemented/
+**Name**: {1}
+**Armory**: {2}
+**RaiderIO**: {3}
+**Logs**: {4}
 
-*Why the applicant wants to join*
+**Why the applicant wants to join**
+{5}
 
-Not implemented
+**Experience so far**
+{6}
 
-*Experience so far*
-
-Not implemented
-
-*Status*
-
-Accepted
+**Status**
+Pending
 ** **
-""".format(ctx.author))
+""".format(datetime.now().strftime("%D"), ctx.author, applicant(applicants, ctx.author)["armory"], applicant(applicants, ctx.author)["raiderio"], applicant(applicants, ctx.author)["logs"], applicant(applicants, ctx.author)["why"], applicant(applicants, ctx.author)["xp"]))
+
 
 @bot.command(pass_context=True)
 @commands.has_any_role('Hand', 'Crusader', 'Sentinel', 'High Hand', 'Councillor')
+@commands.cooldown(2, 5, commands.BucketType.user)
 async def friend(ctx):
     role = discord.utils.get(bot.get_guild(238705194244898817).roles, name='Friend')
     member = ctx.message.mentions[0]
@@ -77,21 +145,41 @@ async def friend(ctx):
         await ctx.send("A friend of yours is a friend of mine!")
 
 @bot.command(pass_context=True)
+@commands.has_any_role('Councillor')
+@commands.cooldown(2, 5, commands.BucketType.user)
+async def accept(ctx):
+    role = discord.utils.get(bot.get_guild(238705194244898817).roles, name='Initiate')
+    member = ctx.message.mentions[0]
+    channel = bot.get_channel(651182846207197185)
+
+    await member.add_roles(role)
+    await channel.send("""**Application** ({0})
+
+    **Name**: {1}
+
+    **Status**
+    Accepted
+    ** **
+    """.format(datetime.now().strftime("%D"), member))
+
+
+@bot.command(pass_context=True)
 @commands.cooldown(1, 180, commands.BucketType.user)
 async def rules(ctx):
-    await ctx.send("""
+    await ctx.author.send("""
 Our rules can be navigated with a menu based system. Please see the following options:
 
 ```
 !ranks       - Types of ranks in the guild.
 !times       - When Silverblade raids.
 !prep        - Being prepared for raiding.
-!addons      - We have a set of **mandatory addons**
+!addons      - We have a set of mandatory addons
 !application - Becoming a raider is application based.
 !alts        - We welcome alts in Silverblade.
 !friend      - We allow our guild members to invite friends.
 ```
 """)
+
 
 @bot.command(pass_context=True)
 @commands.cooldown(1, 180, commands.BucketType.user)
@@ -124,6 +212,7 @@ In other words: As a Trial/Initiate consider yourself a raider, but without the 
 Failing the trial process gets you demoted to Social.
 """)
 
+
 @bot.command(pass_context=True)
 @commands.cooldown(1, 180, commands.BucketType.user)
 async def alts(ctx):
@@ -136,6 +225,7 @@ We will not invite your alt just because you feel like playing it on that partic
 
 This is primarily because of forced personal loot, because we don't want items spreading across many characters being played by one person. We prefer more loot to go to one character for improved output, leading to improved progress.
 """)
+
 
 @bot.command(pass_context=True)
 @commands.cooldown(1, 180, commands.BucketType.user)
@@ -166,19 +256,21 @@ That means:
 Vantus runes may be provided when needed. If feast is the best type of food at the time, feasts may also be provided, but you should still have food so we don't have to pop an entire table for one guy.
 """)
 
+
 @bot.command(pass_context=True)
 @commands.cooldown(1, 180, commands.BucketType.user)
 async def addons(ctx):
     await ctx.send("""
-These addons are mandatory:
+These add-ons are mandatory:
 
 1. Deadly Boss Mods OR BigWigs
 2. RCLootCouncil
 3. Exorsus Raid Tools - Enable Note in /ert -> Note.
 4. WeakAuras
 
-Keep them updated!
+Please keep them updated!
 """)
+
 
 @bot.command(pass_context=True)
 @commands.cooldown(1, 180, commands.BucketType.user)
@@ -199,6 +291,7 @@ Try to be online 15 minutes before raid. Be on no later than the start and be re
 - If you cannot come at all. Write in the Discord #absences channel
 """)
 
+
 @bot.command(pass_context=True)
 async def about(ctx):
     await ctx.send("""
@@ -212,5 +305,6 @@ If raiding with us is something you're interested in, hit the apply button and t
 
 Please take a look at the `!rules` before applying
 """)
+
 
 bot.run(sys.argv[1])
