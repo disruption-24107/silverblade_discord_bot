d56f9baef2b3f253310632030e851fc9fd01c5e2
Christopher Webb <31657038+cwebbtw@users.noreply.github.com>
2019-12-11 22:33:30 +0000

    Update bot_main.py

--- bot_main.py
+++ bot_main.py
@@ -12,11 +12,17 @@ RANKS_TEXT = Path("resources/ranks.txt").read_text()
 TIMES_TEXT = Path("resources/times.txt").read_text()
 APPLICATION_TEXT = Path("resources/application.txt").read_text()
 APPLICATION_PUBLIC_TEXT = Path("resources/application_public.txt").read_text()
+APPLICATION_ACCEPTED_TEXT = Path("resources/application_accepted.txt").read_text()
+APPLICATION_REJECTED_TEXT = Path("resources/application_rejected.txt").read_text()
+APPLICATION_DEFAULT_REJECTION_REASON_TEXT = Path("resources/application_default_rejection_reason.txt").read_text()
 ALTS_TEXT = Path("resources/alts.txt").read_text()
 PREPARATION_1_TEXT = Path("resources/preparation.1.txt").read_text()
 PREPARATION_2_TEXT = Path("resources/preparation.2.txt").read_text()
 ON_MEMBER_JOIN_1_TEXT = Path("resources/on_member_join.1.txt").read_text()
 ON_MEMBER_JOIN_2_TEXT = Path("resources/on_member_join.2.txt").read_text()
+
+APPLICATIONS_CHANNEL = 651719224275894272
+GUILD = 238705194244898817
 
 bot = commands.Bot(command_prefix='!', case_insensitive=True)
 bot.remove_command('help')
@@ -53,7 +59,6 @@ async def armory(ctx):
         applicant(applicants, ctx.author)["armory"] = ctx.message.content.split(None, 1)[1]
         await ctx.author.send("""Thank you for providing your armory information!""")
 
-
 @bot.command()
 @commands.cooldown(1, 1, commands.BucketType.user)
 async def raiderio(ctx):
@@ -64,7 +69,6 @@ async def raiderio(ctx):
         
         applicant(applicants, ctx.author)["raiderio"] = ctx.message.content.split(None, 1)[1]
         await ctx.author.send("""Thank you for providing your RaiderIO information!""")
-
 
 @bot.command()
 @commands.cooldown(1, 1, commands.BucketType.user)
@@ -77,7 +81,6 @@ async def logs(ctx):
         applicant(applicants, ctx.author)["logs"] = ctx.message.content.split(None, 1)[1]
         await ctx.author.send("""Thank you for providing your logs.""")
 
-
 @bot.command()
 @commands.cooldown(1, 1, commands.BucketType.user)
 async def why(ctx):
@@ -89,7 +92,6 @@ async def why(ctx):
         applicant(applicants, ctx.author)["why"] = ctx.message.content.split(None, 1)[1]
         await ctx.author.send("""Thank you for telling us why you want to join.""")
 
-
 @bot.command()
 @commands.cooldown(1, 1, commands.BucketType.user)
 async def xp(ctx):
@@ -100,7 +102,6 @@ async def xp(ctx):
         
         applicant(applicants, ctx.author)["xp"] = ctx.message.content.split(None, 1)[1]
         await ctx.author.send("""Thank you for telling us your experience/history. If you've provided everything then you can type `!done` to submit your application.""")
-
 
 @bot.command()
 @commands.cooldown(1, 5, commands.BucketType.user)
@@ -132,40 +133,22 @@ async def done(ctx):
             await ctx.author.send("""You haven't told us about your raiding history (use `!xp <history>` here to provide it). If you don't want to provide this, just say N/A""")
             return
 
-        guild = bot.get_guild(238705194244898817)
+        guild = bot.get_guild(GUILD)
         role = discord.utils.get(guild.roles, name='Applicant')
         await guild.get_member(ctx.author.id).add_roles(role)
         
         applicant_map["done"] = datetime.now().strftime("%D")
 
         await ctx.author.send("""Great! I'll post your details in #applications, head over there to check the status""")
-        channel = bot.get_channel(651719224275894272)
-        await channel.send("""**Application** ({0})
-
-**Name**: {1}
-**Armory**: {2}
-**RaiderIO**: {3}
-**Logs**: {4}
-
-**Why the applicant wants to join**
-{5}
-
-**Experience so far**
-{6}
-
-**Status**
-Pending
-** **
-""".format(datetime.now().strftime("%D"), ctx.author, applicant(applicants, ctx.author)["armory"],
-           applicant(applicants, ctx.author)["raiderio"], applicant(applicants, ctx.author)["logs"],
-           applicant(applicants, ctx.author)["why"], applicant(applicants, ctx.author)["xp"]))
+        channel = bot.get_channel(APPLICATIONS_CHANNEL)
+        await channel.send(APPLICATION_ACCEPTED_TEXT.format(datetime.now().strftime("%D"), ctx.author, applicant(applicants, ctx.author)["armory"], applicant(applicants, ctx.author)["raiderio"], applicant(applicants, ctx.author)["logs"], applicant(applicants, ctx.author)["why"], applicant(applicants, ctx.author)["xp"]))
 
 
 @bot.command(pass_context=True)
 @commands.has_any_role('Hand', 'Crusader', 'Sentinel', 'High Hand', 'Councillor')
 @commands.cooldown(2, 5, commands.BucketType.user)
 async def friend(ctx):
-    role = discord.utils.get(bot.get_guild(238705194244898817).roles, name='Friend')
+    role = discord.utils.get(bot.get_guild(GUILD).roles, name='Friend')
     member = ctx.message.mentions[0]
 
     if role in member.roles:
@@ -178,58 +161,31 @@ async def friend(ctx):
 @commands.has_any_role('Councillor')
 @commands.cooldown(2, 5, commands.BucketType.user)
 async def accept(ctx):
-    role = discord.utils.get(bot.get_guild(238705194244898817).roles, name='Initiate')
+    role = discord.utils.get(bot.get_guild(GUILD).roles, name='Initiate')
     member = ctx.message.mentions[0]
-    channel = bot.get_channel(651719224275894272)
-    
-    member = ctx.message.mentions[0]
+    channel = bot.get_channel(APPLICATIONS_CHANNEL)
     await ctx.send("Alright, I'll accept {0} and make them an initiate =)".format(member))
-
     await member.add_roles(role)
-    await channel.send("""**Application** ({0})
-
-    **Name**: {1}
-
-    **Status**
-    Accepted
-    
-    **Information**
-    We've assigned you the 'Initiate' role which you'll have for 4 weeks so that both you and the guild can see if we're a good fit together.
-    
-    You can type `!roles` to understand more about the roles in the guild.
-    
-    ** **
-    """.format(datetime.now().strftime("%D"), member.mention()))
+    await channel.send(APPLICATION_ACCEPTED_TEXT.format(datetime.now().strftime("%D"), member.mention()))
 
 @bot.command(pass_context=True)
 @commands.has_any_role('Councillor')
 @commands.cooldown(2, 5, commands.BucketType.user)
 async def reject(ctx):
-    role = discord.utils.get(bot.get_guild(238705194244898817).roles, name='Applicant')
+    role = discord.utils.get(bot.get_guild(GUILD).roles, name='Applicant')
     member = ctx.message.mentions[0]
     
     if role not in member.roles:
         await ctx.send("Uh oh, {0} isn't an applicant.".format(member))
         return
-    
-    await ctx.send("Alright, I'll decline {0} and remove their applicant role =(".format(member))
-    
-    reason = "Please check back in the future and we will happily review again."
+
+    reason = APPLICATION_DEFAULT_REJECTION_REASON_TEXT
     if len(ctx.message.content.strip().split(" ")) >= 3:
         reason = ctx.message.content.split(None, 2)[2]
-
+        
+    await ctx.send("Alright, I'll decline {0} and remove their applicant role =(".format(member))
     await member.remove_roles(role)
-    await member.send("""Hey {0},
-
-Unfortunately we're not going to be progressing forward with your application to join Silverblade at this time.
-
-{1}
-
-Kind Regards,
-
-- Silverblade Council 
-** **
-""".format(member, reason))
+    await member.send(APPLICATION_REJECTED_TEXT.format(member, reason))
     
 @bot.command(pass_context=True)
 @commands.cooldown(1, 5, commands.BucketType.user)
